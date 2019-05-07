10a5f4b901c185dc913d9de6e13a4dc2c2a34792
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-05-07 23:03:14 +0700

    Override accept

--- compiler/psi/src/org/jetbrains/kotlin/psi/KtQuotation.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/KtQuotation.kt
@@ -8,7 +8,7 @@ package org.jetbrains.kotlin.psi
 import com.intellij.lang.ASTNode
 
 public class KtQuotation(node: ASTNode) : KtExpressionImpl(node) {
-    private lateinit var realPsi: KtElement
+    private lateinit var realPsi: KtDotQualifiedExpression
     private val converter = KastreeConverter()
 
     public fun initializeRealPsi() {
@@ -16,6 +16,9 @@ public class KtQuotation(node: ASTNode) : KtExpressionImpl(node) {
         val factory = KtPsiFactory(node.psi.project, true)
         val parsed = factory.createFile(quotationContext)
         val stringRepresentation = converter.convertFile(parsed).toString()
-        realPsi = factory.createExpression(stringRepresentation)
+        realPsi = factory.createExpression(stringRepresentation) as KtDotQualifiedExpression
     }
+
+    override fun <R, D> accept(visitor: KtVisitor<R, D>, data: D): R =
+        if (::realPsi.isInitialized) visitor.visitDotQualifiedExpression(realPsi, data) else visitor.visitQuotation(this, data)
 }
