65440836ee218b94f407c40191eb02b89ba0d1c3
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-07-09 20:06:17 +0300

    Collect external names

--- compiler/psi/src/org/jetbrains/kotlin/psi/KtQuotation.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/KtQuotation.kt
@@ -7,28 +7,49 @@ package org.jetbrains.kotlin.psi
 
 import com.intellij.lang.ASTNode
 import java.lang.IllegalStateException
+import java.lang.StringBuilder
 
 class KtQuotation(node: ASTNode) : KtExpressionImpl(node) {
-    lateinit var realPsi: KtDotQualifiedExpression
     private lateinit var factory: KtPsiFactory
     private val converter = KastreeConverter()
+
+    lateinit var realPsi: KtDotQualifiedExpression
+    private val externalNames = mutableListOf<String>()
 
     fun initializeRealPsi() {
         if (!::factory.isInitialized) {
             factory = KtPsiFactory(node.psi.project, false)
         }
-        val quotationContent = node.firstChildNode.treeNext.text
+        val quotationContent = createQuotationContent()
         // TODO: quotation content may be not expression
-        val parsed = factory.createExpression(quotationContent)
+        val parsed = factory.createExpressionIfPossible(quotationContent)
 
         try {
-            val converted = converter.convertExpr(parsed)
+            val converted = converter.convertExpr(parsed as KtExpression)
             realPsi = factory.createExpression(converted.toCode()) as KtDotQualifiedExpression
 
-        } catch (_: IllegalStateException) {
-            return
+        } catch (e: Exception) {
+            when (e) {
+                is IllegalStateException, is ClassCastException -> return
+                else -> throw e
+            }
         }
     }
 
     override fun <R, D> accept(visitor: KtVisitor<R, D>, data: D): R = visitor.visitQuotation(this, data)
+
+    private fun createQuotationContent(): String {
+        externalNames.clear()
+        val text = StringBuilder()
+        for (child in children) {
+            if (child is KtSimpleNameStringTemplateEntry || child is KtBlockStringTemplateEntry) {
+                val content = child.firstChild.nextSibling.text
+                externalNames.add(content)
+                text.append(content)
+                continue
+            }
+            text.append(child.text)
+        }
+        return text.toString()
+    }
 }
