999970121410575f84790f4ba6834cee72d6481f
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-08-13 03:32:29 +0300

    Allow macro annotation arguments

--- compiler/frontend/src/org/jetbrains/kotlin/psi/macros/MacroExpanderImpl.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/psi/macros/MacroExpanderImpl.kt
@@ -8,7 +8,11 @@ package org.jetbrains.kotlin.psi.macros
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.resolve.BindingTrace
 import org.jetbrains.kotlin.resolve.constants.evaluate.ConstantExpressionEvaluator
+import org.jetbrains.kotlin.types.TypeUtils
+import java.lang.IllegalArgumentException
+import java.lang.reflect.Constructor
 import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import java.net.URL
 import java.net.URLClassLoader
 import kotlin.meta.Node
@@ -18,24 +22,25 @@ class MacroExpanderImpl(
     private val constantExpressionEvaluator: ConstantExpressionEvaluator,
     dependencies: Collection<String>
 ) : MacroExpander {
-    private val classLoader: URLClassLoader
-
-    init {
-        val urls = dependencies
-            .map { URL("file:///$it") }
-            .toTypedArray()
-        classLoader = URLClassLoader(urls, this::class.java.classLoader)
-    }
+    private val urls: Array<URL> = dependencies
+        .map { URL("file:///$it") }
+        .toTypedArray()
 
     override fun run(annotationEntry: KtAnnotationEntry, node: Node): Node? {
         try {
             val name = annotationEntry.fullName() ?: return null
+            val classLoader = URLClassLoader(urls, this::class.java.classLoader)
             val klass = classLoader.loadClass(name)
-            val ctor = klass.declaredConstructors[0].apply { isAccessible = true }
-            val applyMethods = klass.declaredMethods.filter { it.name == "invoke" }
-            // TODO: Overload resolution
-            val method = applyMethods.getOrNull(0)?.apply { isAccessible = true } ?: return null
-            return method.invoke(ctor.newInstance(), node) as Node
+            val args = annotationEntry.valueArguments
+                .map {
+                    val constant = it.getArgumentExpression()
+                        ?.let { e -> constantExpressionEvaluator.evaluateToConstantValue(e, trace, TypeUtils.NO_EXPECTED_TYPE) }
+                    constant?.value
+                }
+                .toTypedArray()
+            val instance = klass.declaredConstructors.newInstance(args)
+            val invokeMethod = klass.declaredMethods.invokeMethod().apply { isAccessible = true }
+            return invokeMethod.invoke(instance, node) as Node
 
         } catch (e: ClassNotFoundException) {
             // TODO: Report an error
@@ -59,4 +64,26 @@ class MacroExpanderImpl(
             else -> null
         }
     }
+
+    // We cannot choose constructor according to parameter types because
+    // resolving calls is unavailable here
+    private fun Array<Constructor<*>>.newInstance(args: Array<Any?>): Any {
+        val filtered = this
+            .filter { it.parameters.size == args.size }
+            .onEach { it.isAccessible = true }
+        if (args.isEmpty()) return filtered.single().newInstance()
+        var instance: Any
+        for (ctor in filtered) {
+            try {
+                instance = ctor.newInstance(*args)
+                return instance
+            } catch (e: Exception) {
+            }
+        }
+        throw IllegalArgumentException("No matching constructor found")
+    }
+
+    // TODO: Overload resolution
+    private fun Array<Method>.invokeMethod(): Method = singleOrNull { it.name == "invoke" }
+        ?: throw NoSuchMethodException("No invoke method for macro invocation")
 }
