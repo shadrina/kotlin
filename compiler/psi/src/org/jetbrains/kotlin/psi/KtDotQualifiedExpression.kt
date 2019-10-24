0cec13cc46b0addf30fdbcb0d600b4a7ef16ad1d
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-10-24 18:25:46 +0700

    Temporary fix: do not throw UninitializedPropertyException from KtDotQualifiedExpression.getHiddenElement

--- compiler/psi/src/org/jetbrains/kotlin/psi/KtDotQualifiedExpression.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/KtDotQualifiedExpression.kt
@@ -27,7 +27,7 @@ import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes.INSIDE_DIRECTI
 class KtDotQualifiedExpression : KtExpressionImplStub<KotlinPlaceHolderStub<KtDotQualifiedExpression>>, KtQualifiedExpression,
     KtReplaceable {
     override var replacedElement: KtElement = this
-    override lateinit var hiddenElement: KtElement
+    override var hiddenElement: KtElement = this
     override lateinit var metaTools: MetaTools
     override val hasHiddenElementInitialized = false
     override var isHidden = false
@@ -38,7 +38,9 @@ class KtDotQualifiedExpression : KtExpressionImplStub<KotlinPlaceHolderStub<KtDo
 
     override fun createHiddenElementFromContent(content: String): KtElement = TODO()
 
-    constructor(node: ASTNode) : super(node)
+    constructor(node: ASTNode) : super(node) {
+        metaTools = MetaTools(node)
+    }
 
     constructor(stub: KotlinPlaceHolderStub<KtDotQualifiedExpression>) : super(stub, KtStubElementTypes.DOT_QUALIFIED_EXPRESSION)
 
